25e6a118d23bbf3c4956cfe750904a96e3b776ab
hildaguevara <56275367+hildaguevara@users.noreply.github.com>
2020-10-05 11:47:48 -0600

    Commit new branch

--- src/services/index.js
+++ src/services/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const conexion = (name, surname) => 
-    axios.get(`http://127.0.0.1/proyecto/getUser.php`)
+    axios.get(`http://127.0.0.1:81/proyecto/getUser.php`)
     .then(response => {
         console.log(response)
         return Promise.resolve(response.data)
